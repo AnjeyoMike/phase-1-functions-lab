3eec17b20b6686bfb3d97e31583eb3ab354f65c3
Anjeyo Mike <mike.matekeya@student.moringaschool.com>
2024-04-01 13:01:34 +0300

    my fifth lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    const hqLocation = 42; // Headquarters location
+    return Math.abs(pickupLocation - hqLocation);
+}
+
+
+function distanceFromHqInFeet(pickupLocation) {
+    const feetPerBlock = 264;
+    return distanceFromHqInBlocks(pickupLocation) * feetPerBlock;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+    const feetPerBlock = 264;
+    return Math.abs(endBlock - startBlock) * feetPerBlock;
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+    const distance = distanceTravelledInFeet(startBlock, endBlock);
+    
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
